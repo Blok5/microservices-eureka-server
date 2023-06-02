0f6c635597634775eb98f0ef63a8cc8b91edb5d0
Igor Simakov <i.simakov@banki.ru>
2023-06-02 12:26:21 +0800

    add microservices eureka server

---
+++ src/main/java/ru/simakov/configuration/EurekaServerConfiguration.java
@@ -0,0 +1,9 @@
+package ru.simakov.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaServerConfiguration {
+}
